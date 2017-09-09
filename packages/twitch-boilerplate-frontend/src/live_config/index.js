26a379b09c8bdda3ea4549e97722694d88f27e7f
Alan Love <alan@cattes.us>
2017-09-09 17:28:35 -0500

    renamed package names to be more generic

--- packages/twitch-boilerplate-frontend/src/live_config/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import LiveConfig from 'liveconfig/LiveConfig';
-
-const mainElement = document.getElementById("twitch-extension-live-config");
-
-ReactDOM.render(<Viewer />, mainElement);
